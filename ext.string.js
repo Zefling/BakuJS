79f07268c538c1f67b40daffb26fd9eadb35b432
Zéfling <zefling@ikilote.net>
2016-01-11 00:38:34 +0100

    Update ext.string.js
    Mise en forme du fichier

--- ext.string.js
+++ ext.string.js
@@ -2,19 +2,18 @@
 
 /**
  * formatage par function et paramètres :
- * - {clé, function, params}
+ * - {key, function, params}
  * @params liste d'arguments, array ou object
  * @return string
  */
 String.prototype.format = function (){
-  var args = arguments;
-  if (typeof args[0] === 'array' || typeof args[0] === 'object') {
-    args = args[0];
-  }  
+	var args = arguments;
+	if (typeof args[0] === 'array' || typeof args[0] === 'object') {
+		args = args[0];
+	}  
 	return this.replace(/{\s*([^,}]+)\s*(?:,\s*([^,}]+)\s*)?(?:,\s*([^,}]+)\s*)?}/g, function (base, value, func, params) {
-    console.log(func !== undefined, typeof eval(func) === 'function');
-    return (func !== undefined && typeof eval(func) === 'function') 
-      ? ( params !== undefined ? eval(func)(args[value], params) : eval(func)(args[value]) )
-      : args[value];
+		return func !== undefined && typeof eval(func) === 'function' 
+			? ( params !== undefined ? eval(func)(args[value], params) : eval(func)(args[value]) )
+			: args[value];
 	});
 };
