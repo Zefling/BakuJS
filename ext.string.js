063c7cdc8c4ca54d0ab3138b7747299387fc2aee
Zéfling <zefling@ikilote.net>
2016-01-11 22:23:40 +0100

    Update ext.string.js
    String.prototype.format
    - Supporte les \} pour les cas comme celui-ci : {0, test, @parm{0\} }
    - Utilise l'objet Formater pour fonctionner

--- ext.string.js
+++ ext.string.js
@@ -1,8 +1,11 @@
+/** formateur (object avec des fonctions : "func(mixed value):string" ou "func(mixed value, string params):string"   */
+var Formater = {};
 
 
 /**
  * formatage par function et paramètres :
  * - {key, function, params}
+ * les functions sont des méthodes de Formater
  * @params liste d'arguments, array ou object
  * @return string
  */
@@ -11,9 +14,9 @@ String.prototype.format = function (){
 	if (typeof args[0] === 'array' || typeof args[0] === 'object') {
 		args = args[0];
 	}  
-	return this.replace(/{\s*([^,}]+)\s*(?:,\s*([^,}]+)\s*)?(?:,\s*([^,}]+)\s*)?}/g, function (base, value, func, params) {
-		return func !== undefined && typeof eval(func) === 'function' 
-			? ( params !== undefined ? eval(func)(args[value], params) : eval(func)(args[value]) )
+	return this.replace(/{\s*([^,}]+)\s*(?:,\s*([^,}]+)\s*)?(?:,\s*((?:\\.|[^}])+)\s*)?}/g, function (base, value, func, params) {
+		return (func !== undefined && typeof Formater[func] === 'function')  
+			? ( params !== undefined ? Formater[func](args[value], params.replace('\\}', '}')) : Formater[func](args[value]) )
 			: args[value];
 	});
 };
