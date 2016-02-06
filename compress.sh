804b51f88c7f2695dfbcb603965b9dcefd215b8d
zefling <zefling@ikilote.net>
2016-02-06 01:52:04 +0100

    minify : change yui-compressor for closure-compiler

--- compress.sh
+++ compress.sh
@@ -3,11 +3,11 @@
 name=''
 if [ "$1" = 'full' ]; then
 	name='.full'
-	cat baku.js baku.math.js baku.number.js baku.date.js baku.string.js baku.string.formater.js baku.dom.js  | yui-compressor --type js -o "baku$name.min.js"
+	cat baku.js baku.math.js baku.number.js baku.date.js baku.regexp.js baku.string.js baku.string.formater.js baku.dom.js  | closure-compiler --js_output_file="baku$name.min.js"
 elif [ "$1" = 'dom' ]; then
 	name='.dom'
-	cat baku.js baku.dom.js | yui-compressor --type js -o "baku$name.min.js"
+	cat baku.js baku.dom.js | closure-compiler --js_output_file="baku$name.min.js"
 else 
-	cat baku.js baku.math.js baku.number.js baku.date.js baku.string.js baku.string.formater.js | yui-compressor --type js -o "baku$name.min.js"
+	cat baku.js baku.math.js baku.number.js baku.date.js baku.regexp.js baku.string.js baku.string.formater.js | closure-compiler --js_output_file="baku$name.min.js"
 fi
-sed -i '1i/*! BakuJs v0.1b | (c) Zefling | license, see: github.com/Zefling/BakuJS */' "baku$name.min.js"
+sed -i '1i/*! BakuJs v0.1d | (c) Zefling | license, see: github.com/Zefling/BakuJS */' "baku$name.min.js"
